9a568abaaf259a9b9afbae4a820c618265fe356c
SakeshKaranjit <137280465+SakeshKaranjit@users.noreply.github.com>
2023-11-27 17:08:23 +0545

    Update webapp.go
    Commit according to webapp in ca

--- webapp.go
+++ webapp.go
@@ -3,18 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+func hosthandler(w http.ResponseWriter, r *http.Request) {
+	name, err :=os.Hostname()
+
+	if err !=nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "<h1>HOSTNAME: %s</h1><br>", name)
+	fmt.Fprintf(w, "<h1>ENVIRONMENT VARS:</h1><br>")
+	fmt.Fprintf(w, "<ul>")
+
+	for _, evar := range os.Environ() {
+		fmt.Fprintf(w, "<li>%s</li>", evar)
+	}
+	fmt.Fprintf(w, "</ul>")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "<h1> Awesome site in Go!</h1><br>")
+	fmt.Fprintf(w, "<a href='/host/'>Host info</a><br>")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-
-	fmt.Println("Server is listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/host/", hostHandler)
+	http.ListenAndServe(":8080", nil)
 }
+
+// Compile with:
+// env GOARCH=386 GOOS=linux go build webapp.go
